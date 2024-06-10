c7057648169b5662ba621f469e2989f58fa3705c
Murat Kaya <91426358+mkaya26@users.noreply.github.com>
2024-06-11 00:35:30 +0300

    Add project files.

---
+++ ERPServer/ERPServer.Infrastructure/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using ERPServer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ERPServer.Infrastructure.Configurations
+{
+    internal sealed class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.Property(p => p.Name).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(p => p.TaxDepartment).HasColumnType("varchar(150)");
+            builder.Property(p => p.TaxNumber).HasColumnType("varchar(11)");
+            builder.Property(p => p.City).HasColumnType("varchar(100)");
+            builder.Property(p => p.Town).HasColumnType("varchar(100)");
+            builder.Property(p => p.FullAddress).HasColumnType("varchar(250)");
+        }
+    }
+}
---
+++ ERPServer/ERPServer.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using ERPServer.Domain.Entities;
+using GenericRepository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERPServer.Infrastructure.Context
+{
+    internal sealed class ApplicationDbContext : IdentityDbContext<AppUser, IdentityRole<Guid>, Guid>, IUnitOfWork
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(typeof(DependencyInjection).Assembly);
+
+            builder.Ignore<IdentityUserLogin<Guid>>();
+            builder.Ignore<IdentityRoleClaim<Guid>>();
+            builder.Ignore<IdentityUserToken<Guid>>();
+            builder.Ignore<IdentityUserRole<Guid>>();
+            builder.Ignore<IdentityUserClaim<Guid>>();
+        }
+    }
+}
